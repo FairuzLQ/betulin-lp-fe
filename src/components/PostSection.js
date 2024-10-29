0a35c4fcb32c305bee759160360d3777f7b42197
rakakalbu <rakakalbuimaji@gmail.com>
2024-10-29 17:17:23 +0700

    clickable

--- src/components/PostSection.js
+++ src/components/PostSection.js
@@ -1,20 +1,41 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaFacebook, FaTwitter, FaInstagram, FaShareAlt } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import '../styles/PostSection.css';
 
 const PostSection = ({ post }) => {
+  const [randomArticles, setRandomArticles] = useState([]);
+  const apiUrl = process.env.REACT_APP_API_URL || '';
+
   useEffect(() => {
-    AOS.init({
-      duration: 1000,
-      once: true,
-    });
-  }, []);
+    AOS.init({ duration: 1000, once: true });
+    
+    const fetchRandomArticles = async () => {
+      try {
+        const totalResponse = await fetch(`${apiUrl}/api/artikels?pagination[pageSize]=1`);
+        const totalData = await totalResponse.json();
+        const totalArticles = totalData.meta.pagination.total;
+        const totalPages = Math.ceil(totalArticles / 3);
+        const randomPage = Math.floor(Math.random() * totalPages) + 1;
+
+        const response = await fetch(
+          `${apiUrl}/api/artikels?populate=*&pagination[page]=${randomPage}&pagination[pageSize]=3`
+        );
+        const data = await response.json();
+        
+        setRandomArticles(data.data);
+      } catch (error) {
+        console.error('Error fetching random articles:', error);
+      }
+    };
+
+    fetchRandomArticles();
+  }, [apiUrl]);
 
   if (!post) return null;
 
-  // Copy link function
   const copyLinkToClipboard = () => {
     const postUrl = window.location.href;
     const postDate = new Date(post.TglArtikel).toLocaleDateString();
@@ -22,35 +43,21 @@ const PostSection = ({ post }) => {
     const textToCopy = `${postUrl}\n\n${shareText}`;
 
     navigator.clipboard.writeText(textToCopy)
-      .then(() => {
-        alert("Link and message copied to clipboard!");
-      })
-      .catch((error) => {
-        console.error("Error copying text to clipboard:", error);
-      });
+      .then(() => alert("Link and message copied to clipboard!"))
+      .catch((error) => console.error("Error copying text to clipboard:", error));
   };
 
-    // Check if REACT_APP_API_URL is defined
-    const apiUrl = process.env.REACT_APP_API_URL || '';
-    const featuredImageUrl = post.FeaturedImage?.formats?.large?.url
-      ? `${apiUrl}${post.FeaturedImage.formats.large.url}`
-      : post.FeaturedImage?.url
-      ? `${apiUrl}${post.FeaturedImage.url}`
-      : null;
-
-  // Dummy Data for trending posts
-  const trendingPosts = [
-    { id: 1, title: "Maximizing Small Spaces", author: "admin", date: "Jumat, 14 Oktober 2024", image: "https://via.placeholder.com/300x200?text=Small+Spaces" },
-    { id: 2, title: "Choosing the Best Paint Colors", author: "admin", date: "Kamis, 13 Oktober 2024", image: "https://via.placeholder.com/300x200?text=Paint+Colors" },
-    { id: 3, title: "The Future of Smart Homes", author: "admin", date: "Sabtu, 15 Oktober 2024", image: "https://via.placeholder.com/300x200?text=Smart+Homes" },
-  ];
+  const featuredImageUrl = post.FeaturedImage?.formats?.large?.url
+    ? `${apiUrl}${post.FeaturedImage.formats.large.url}`
+    : post.FeaturedImage?.url
+    ? `${apiUrl}${post.FeaturedImage.url}`
+    : null;
 
   return (
     <section className="post-section-page" data-aos="fade-up">
       <div className="post-section-content-wrapper">
         <div className="post-section-left" data-aos="fade-up">
           <p className="post-section-category">{post.kategori_artikel?.NamaKategori}</p>
-          
           <h1 className="post-section-title">{post.TitleArtikel}</h1>
 
           {featuredImageUrl && (
@@ -60,17 +67,12 @@ const PostSection = ({ post }) => {
                 alt={post.TitleArtikel}
                 className="post-section-featured-image"
               />
-              <div className='post-section-image-source'>
-                Sumber: {post.SumberFoto}
-              </div>
+              <div className='post-section-image-source'>Sumber: {post.SumberFoto}</div>
             </div>
           )}
 
           <div className="post-section-meta" data-aos="fade-up" data-aos-delay="200">
-            <p>
-              By {post.penulis_artikel?.NamaPenulis} |{' '}
-              {new Date(post.TglArtikel).toLocaleDateString()}
-            </p>
+            <p>By {post.penulis_artikel?.NamaPenulis} | {new Date(post.TglArtikel).toLocaleDateString()}</p>
             <div className="post-section-share-post" onClick={copyLinkToClipboard}>
               <FaShareAlt /> <span>Share this post</span>
             </div>
@@ -87,24 +89,30 @@ const PostSection = ({ post }) => {
 
           <div className="post-section-tags" data-aos="fade-up" data-aos-delay="600">
             {post.tag_artikels.map((tag, index) => (
-              <span key={index} className="post-section-tag">
-                #{tag.NamaTag}
-              </span>
+              <span key={index} className="post-section-tag">#{tag.NamaTag}</span>
             ))}
           </div>
         </div>
 
         <div className="post-section-right" data-aos="fade-left">
           <div className="post-section-trending-posts" data-aos="fade-up" data-aos-delay="200">
-            <h3>Post Terpopuler</h3>
-            {trendingPosts && trendingPosts.map((trendingPost) => (
-              <div className="post-section-trending-post" key={trendingPost.id} data-aos="fade-right" data-aos-delay="400">
-                <img src={trendingPost.image} alt={trendingPost.title} className="post-section-trending-post-image" />
-                <div className="post-section-trending-post-details">
-                  <h4>{trendingPost.title}</h4>
-                  <p className="author-date">{trendingPost.author} | {trendingPost.date}</p>
+            <h3>Baca Juga Ini</h3>
+            {randomArticles.map((randomPost) => (
+              <Link to={`/blog-post/${randomPost.documentId}`} key={randomPost.id} className="post-section-trending-post-link">
+                <div className="post-section-trending-post" data-aos="fade-right" data-aos-delay="400">
+                  <img
+                    src={`${apiUrl}${randomPost.FeaturedImage?.formats?.small?.url || ''}`}
+                    alt={randomPost.TitleArtikel}
+                    className="post-section-trending-post-image"
+                  />
+                  <div className="post-section-trending-post-details">
+                    <h4>{randomPost.TitleArtikel}</h4>
+                    <p className="author-date">
+                      {randomPost.penulis_artikel?.NamaPenulis} | {new Date(randomPost.TglArtikel).toLocaleDateString()}
+                    </p>
+                  </div>
                 </div>
-              </div>
+              </Link>
             ))}
           </div>
 
