cbbe07b76ffc79ff9a599046856838bf0c3a04cb
Fairuz Elqi Mochammad <elqimochammad12@gmail.com>
2024-11-06 12:18:32 +0700

    make interactive icon

--- src/components/FlyingIconsBackground.js
+++ src/components/FlyingIconsBackground.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
+import Draggable from 'react-draggable';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faHammer,
@@ -13,38 +14,36 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import '../styles/FlyingIconsBackground.css';
 
-const FlyingIconsBackground = () => {
-    useEffect(() => {
-        const handleMouseMove = (event) => {
-            const icons = document.querySelectorAll('.floating-icon');
-            const { clientX, clientY } = event;
+const FloatingIconsBackground = () => {
+    const icons = [
+        faHammer,
+        faWrench,
+        faScrewdriver,
+        faBolt,
+        faCogs,
+        faToolbox,
+        faDraftingCompass,
+        faHardHat,
+        faPencilRuler,
+    ];
 
-            icons.forEach((icon, index) => {
-                const speed = (index + 1) * 0.05; // Vary speed for each icon
-                const x = (window.innerWidth / 2 - clientX) * speed;
-                const y = (window.innerHeight / 2 - clientY) * speed;
-
-                icon.style.transform = `translate(${x}px, ${y}px) rotate(${index * 10}deg)`;
-            });
-        };
-
-        window.addEventListener('mousemove', handleMouseMove);
-
-        return () => window.removeEventListener('mousemove', handleMouseMove);
-    }, []);
+    // State to track which icon is currently draggable
+    const [activeIcon, setActiveIcon] = useState(null);
 
     return (
         <div className="icon-background">
-            <FontAwesomeIcon icon={faHammer} className="floating-icon hammer" />
-            <FontAwesomeIcon icon={faWrench} className="floating-icon wrench" />
-            <FontAwesomeIcon icon={faScrewdriver} className="floating-icon screwdriver" />
-            <FontAwesomeIcon icon={faBolt} className="floating-icon bolt" />
-            <FontAwesomeIcon icon={faCogs} className="floating-icon cogs" />
-            <FontAwesomeIcon icon={faToolbox} className="floating-icon toolbox" />
-            <FontAwesomeIcon icon={faDraftingCompass} className="floating-icon compass" />
-            <FontAwesomeIcon icon={faHardHat} className="floating-icon hardhat" />
-            <FontAwesomeIcon icon={faPencilRuler} className="floating-icon ruler" />
-
+            {icons.map((icon, index) => (
+                <Draggable
+                    key={index}
+                    disabled={activeIcon !== index} // Only draggable if it's the active icon
+                    onStart={() => setActiveIcon(index)} // Set this icon as active on drag start
+                    onStop={() => setActiveIcon(null)} // Reset when dragging stops
+                >
+                    <div className={`floating-icon icon-${index}`}>
+                        <FontAwesomeIcon icon={icon} />
+                    </div>
+                </Draggable>
+            ))}
             <div className="content">
                 <h1>Welcome to Our Services</h1>
             </div>
@@ -52,4 +51,4 @@ const FlyingIconsBackground = () => {
     );
 };
 
-export default FlyingIconsBackground;
+export default FloatingIconsBackground;
